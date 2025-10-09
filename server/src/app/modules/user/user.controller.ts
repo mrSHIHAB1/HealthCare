09c2436a535884d457fc3dba9e848f02ba5417c1
mrSHIHAB1 <montasirr36@gmail.com>
2025-10-09 12:21:41 +0600

    cloudinary Image Upload

---
+++ server/src/app/modules/user/user.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { UserService } from "./user.service";
+import sendResponse from "../../shared/sendResponse";
+
+const createPatient=catchAsync(async(req:Request,res:Response)=>{
+   
+ const result=await UserService.createPatient(req);
+
+ sendResponse(res,{
+    statusCode:201,
+    success:true,
+    message:"patien creaed successfully",
+    data:result
+ })
+})
+export const UserController={
+createPatient
+}
