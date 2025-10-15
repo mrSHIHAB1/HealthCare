c52a305c77eb6c39db617adf42a0bb5ffc1e95c6
mrSHIHAB1 <montasirr36@gmail.com>
2025-10-15 22:44:54 +0600

    error catching and sechema add

--- server/src/app/middlewares/auth.ts
+++ server/src/app/middlewares/auth.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from "express"
 import { jwtHelper } from "../helper/jwtHelper";
-
+import ApiError from "../errors/ApiError";
+import httpStatus from "http-status"
 const auth = (...roles: string[]) => {
     return async (req: Request & { user?: any }, res: Response, next: NextFunction) => {
         try {
             const token = req.cookies.accessToken;
 
             if (!token) {
-                throw new Error("You are not authorized!")
+                throw new ApiError(httpStatus.UNAUTHORIZED,"You are not authorized!")
             }
 
             const verifyUser = jwtHelper.verifyToken(token, "abcd");
@@ -15,7 +16,7 @@ const auth = (...roles: string[]) => {
             req.user = verifyUser;
 
             if (roles.length && !roles.includes(verifyUser.role)) {
-                throw new Error("You are not authorized!")
+                throw new ApiError(httpStatus.UNAUTHORIZED,"You are not authorized!")
             }
 
             next();
