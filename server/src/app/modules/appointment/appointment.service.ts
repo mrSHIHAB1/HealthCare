93da57c7ff5d44134e67f6e56cb257672fdf45b0
mrSHIHAB1 <montasirr36@gmail.com>
2025-10-21 01:34:25 +0600

    stripe and prescription

--- server/src/app/modules/appointment/appointment.service.ts
+++ server/src/app/modules/appointment/appointment.service.ts
@@ -1,7 +1,12 @@
+import { AppointmentStatus, Prisma, UserRole } from "@prisma/client";
+import ApiError from "../../errors/ApiError";
 import { stripe } from "../../helper/stripe";
 import { prisma } from "../../shared/prisma";
 import { IJWTPayload } from "../../types/common";
 import { v4 as uuidv4 } from 'uuid';
+
+import httpStatus from 'http-status'
+import { IOptions, paginationHelper } from "../../helper/paginationHelper";
 
 const createAppointment = async (user: IJWTPayload, payload: { doctorId: string, scheduleId: string }) => {
     const patientData = await prisma.patient.findUniqueOrThrow({
@@ -49,23 +54,24 @@ const createAppointment = async (user: IJWTPayload, payload: { doctorId: string,
             }
         })
 
-        const transactionId = uuidv4();
+          const transactionId = uuidv4();
 
-        await tnx.payment.create({
+        const paymentData = await tnx.payment.create({
             data: {
                 appointmentId: appointmentData.id,
                 amount: doctorData.appointmentFee,
                 transactionId
             }
         })
-  const session = await stripe.checkout.sessions.create({
+
+        const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
             mode: "payment",
             customer_email: user.email,
             line_items: [
                 {
                     price_data: {
-                        currency: "usd",
+                        currency: "bdt",
                         product_data: {
                             name: `Appointment with ${doctorData.name}`,
                         },
@@ -74,17 +80,105 @@ const createAppointment = async (user: IJWTPayload, payload: { doctorId: string,
                     quantity: 1,
                 },
             ],
-           
+            metadata: {
+                appointmentId: appointmentData.id,
+                paymentId: paymentData.id
+            },
             success_url: `https://www.programming-hero.com/`,
             cancel_url: `https://next.programming-hero.com/`,
         });
-        return appointmentData;
+
+        return { paymentUrl: session.url };
     })
 
 
     return result;
 };
+const getMyAppointment = async (user: IJWTPayload, filters: any, options: IOptions) => {
+    const { page, limit, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
+    const { ...filterData } = filters;
 
+    const andConditions: Prisma.AppointmentWhereInput[] = [];
+
+    if (user.role === UserRole.PATIENT) {
+        andConditions.push({
+            patient: {
+                email: user.email
+            }
+        })
+    }
+    else if (user.role === UserRole.DOCTOR) {
+        andConditions.push({
+            doctor: {
+                email: user.email
+            }
+        })
+    }
+
+    if (Object.keys(filterData).length > 0) {
+        const filterConditions = Object.keys(filterData).map(key => ({
+            [key]: {
+                equals: (filterData as any)[key]
+            }
+        }))
+
+        andConditions.push(...filterConditions)
+    }
+
+    const whereConditions: Prisma.AppointmentWhereInput = andConditions.length > 0 ? { AND: andConditions } : {};
+
+    const result = await prisma.appointment.findMany({
+        where: whereConditions,
+        skip,
+        take: limit,
+        orderBy: {
+            [sortBy]: sortOrder
+        },
+        include: user.role === UserRole.DOCTOR ?
+            { patient: true } : { doctor: true }
+    });
+
+    const total = await prisma.appointment.count({
+        where: whereConditions
+    });
+
+    return {
+        meta: {
+            total,
+            limit,
+            page
+        },
+        data: result
+    }
+
+}
+const updateAppointmentStatus = async (appointmentId: string, status: AppointmentStatus, user: IJWTPayload) => {
+    const appointmentData = await prisma.appointment.findUniqueOrThrow({
+        where: {
+            id: appointmentId
+        },
+        include: {
+            doctor: true
+        }
+    });
+
+    if (user.role === UserRole.DOCTOR) {
+        if (!(user.email === appointmentData.doctor.email))
+            throw new ApiError(httpStatus.BAD_REQUEST, "This is not your appointment")
+    }
+
+    return await prisma.appointment.update({
+        where: {
+            id: appointmentId
+        },
+        data: {
+            status
+        }
+    })
+
+}
 export const AppointmentService = {
-    createAppointment,
+     createAppointment,
+    getMyAppointment,
+    updateAppointmentStatus
 };
---
+++ server/src/app/modules/payment/payment.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { PaymentService } from "./payment.service";
+import sendResponse from "../../shared/sendResponse";
+import { stripe } from "../../helper/stripe";
+
+const handleStripeWebhookEvent = catchAsync(async (req: Request, res: Response) => {
+
+    const sig = req.headers["stripe-signature"] as string;
+    const webhookSecret = "whsec_e6ad61360c244e2b49b8495743d8d1968459a6307cae50d2828d8575a2029d11"
+
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (err: any) {
+        console.error("⚠️ Webhook signature verification failed:", err.message);
+        return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+    const result = await PaymentService.handleStripeWebhookEvent(event);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Webhook req send successfully',
+        data: result,
+    });
+});
+
+export const PaymentController = {
+    handleStripeWebhookEvent
+}
